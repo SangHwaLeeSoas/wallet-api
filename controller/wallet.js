7c5b09c54a542152207db9ad71a40aa9b52f022f
soas <sangwha0924@gmail.com>
2023-04-25 23:55:45 +0900

    Feat : 지갑 생성, 조회

--- controller/wallet.js
+++ controller/wallet.js
@@ -14,7 +14,6 @@ app.get('/get/balance', async (req, res) => {
   try {
     logger.info(`/get/balance => ${req.query.addr}`)
     const addr = req.query.addr || (() => { throw new customError(resCode.BAD_REQUEST, 'empty addr') })();
-    console.log(addr)
     const balance = await walletService.getBalance(addr)
     const data = {'balance' : balance};
 
@@ -23,22 +22,63 @@ app.get('/get/balance', async (req, res) => {
       data: data
     });
   } catch (e) {
-    if (e instanceof customError) {
-      logger.info(e)
-      res.status(e.httpStatusCode).json({
-        code : e.code,
-        msg : e.message
-      })
-    } else {
-      logger.error(e)
-      const customRes = resCode.create(resCode.SERVER_ERROR)
-      res.status(customRes.httpCode).json({
-        code : customRes.code,
-        msg : e.message
-      })
-    }
+    makeErrorResponse(e, res)
   }
 });
 
+
+/* * 지갑 생성 API */
+app.get('/make/account', async (req, res) => {
+
+  try {
+    logger.info(`/make/account => ${req.query.privateKey}`)
+    const privateKey = req.query.privateKey || (() => { throw new customError(resCode.BAD_REQUEST, 'empty privateKey') })();
+    const wallet = await walletService.makeWallet(privateKey)
+    const data = {'wallet' : wallet};
+
+    res.json({
+      code: 200,
+      data: data
+    });
+  } catch (e) {
+    makeErrorResponse(e, res)
+  }
+});
+
+
+/* * 지갑 목록 전체 조회 API */
+app.get('/get/all/account', async (req, res) => {
+
+  try {
+    const accountList = await walletService.getAccountAll()
+    const data = {'accountList' : accountList};
+
+    res.json({
+      code: 200,
+      data: data
+    });
+  } catch (e) {
+    makeErrorResponse(e, res)
+  }
+});
+
+
+/* * 공통 Error Response */
+function makeErrorResponse(e, res) {
+  if (e instanceof customError) {
+    logger.info(e)
+    res.status(e.httpStatusCode).json({
+      code : e.code,
+      msg : e.message
+    })
+  } else {
+    logger.error(e)
+    const customRes = resCode.create(resCode.SERVER_ERROR)
+    res.status(customRes.httpCode).json({
+      code : customRes.code,
+      msg : e.message
+    })
+  }
+}
 
 module.exports = app;
--- service/wallet.js
+++ service/wallet.js
@@ -11,17 +11,65 @@ const rpcUrl = properties.RPC_PROTOCOL + properties.PROTOCOL_SEPARATE + properti
 const provider = new Web3.providers.HttpProvider(rpcUrl);
 const web3 = new Web3(provider);
 
+/* * Smart Contract 세팅 */
+const rawAbi = fs.readFileSync(properties.ABI_PATH);
+const tokenAbi = JSON.parse(rawAbi.toString());
+const myToken = new web3.eth.Contract(tokenAbi, properties.CONTRACT_ADDRESS);
 
 /* * 잔액 조회 */
 exports.getBalance = async (addr) => {
     try {
-        const rawAbi = fs.readFileSync(properties.ABI_PATH);
-        const tokenAbi = JSON.parse(rawAbi.toString());
-        const myToken = new web3.eth.Contract(tokenAbi, '0x60deb954bc80eb959736a0d0bfbaee2e2e767de7');
         const balance = await myToken.methods.balanceOf(addr).call()
         return balance;
     } catch (e) {
-        throw new customError(resCode.BAD_REQUEST, e.message)
+        const code = e.code;
+        switch (code) {
+            case "INVALID_ARGUMENT":
+                throw new customError(resCode.INVALID_ADDRESS, e.message)
+            case "OUT_OF_GAS" :
+                throw new customError(resCode.OUT_OF_GAS, e.message)
+            default :
+                throw new customError(resCode.RPC_ERROR, e.message)
+        }
+    }
+}
+
+
+/* * 지갑 생성 */
+exports.makeWallet = async (privateKey) => {
+    try {
+        const wallet = await web3.eth.personal.newAccount(privateKey)
+        console.log(wallet)
+        return wallet;
+    } catch (e) {
+        const code = e.code;
+        switch (code) {
+            case "INVALID_ARGUMENT":
+                throw new customError(resCode.INVALID_ADDRESS, e.message)
+            case "OUT_OF_GAS" :
+                throw new customError(resCode.OUT_OF_GAS, e.message)
+            default :
+                throw new customError(resCode.RPC_ERROR, e.message)
+        }
+    }
+}
+
+/* * 지갑 목록 조회 */
+exports.getAccountAll = async () => {
+    try {
+        const account = await web3.eth.getAccounts()
+        console.log(account)
+        return account;
+    } catch (e) {
+        const code = e.code;
+        switch (code) {
+            case "INVALID_ARGUMENT":
+                throw new customError(resCode.INVALID_ADDRESS, e.message)
+            case "OUT_OF_GAS" :
+                throw new customError(resCode.OUT_OF_GAS, e.message)
+            default :
+                throw new customError(resCode.RPC_ERROR, e.message)
+        }
     }
 }
 
